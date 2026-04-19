7a12bcc213b7b545bbdfba0a7a3bcc0b5a05bccd
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 14:11:55 +0400

    str and first and last strings are declared

---
+++ src/T15Sandwich.java
@@ -0,0 +1,9 @@
+public class T15Sandwich {
+    public static void main(String[] args){
+        String str = "breadjambread";
+        int first = str.indexOf("bread");
+        int last = str.lastIndexOf("bread");
+
+
+    }
+}
