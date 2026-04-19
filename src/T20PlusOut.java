2472fb823fe3d90a717982ba22a6612752df1903
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 15:29:28 +0400

    if statement is written inside the loop

--- src/T20PlusOut.java
+++ src/T20PlusOut.java
@@ -3,6 +3,11 @@ public class T20PlusOut {
         String str = "12xy34";
 
         for (int i =0; i<str.length()-1;i++){
+            if (str.charAt(i) == 'x' && str.charAt(i+1) == 'y' ){
+                System.out.println(str.charAt(i));
+            }else {
+                System.out.println("+");
+            }
 
 
         }
