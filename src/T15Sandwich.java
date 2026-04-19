d9fbad5b350dc0b3d479198bd99cb108d0c5b699
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 14:14:05 +0400

    middle string is declared

--- src/T15Sandwich.java
+++ src/T15Sandwich.java
@@ -4,6 +4,9 @@ public class T15Sandwich {
         int first = str.indexOf("bread");
         int last = str.lastIndexOf("bread");
 
+        String middle =str.substring(first +5,last);
+
+
 
     }
 }
