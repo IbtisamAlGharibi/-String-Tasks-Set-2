d3ae4d354937c9d087fc3c2694de91f44addc049
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 11:57:32 +0400

    counter is declared and if statement is written inside the loop to count the prefix

--- src/T13PrefixAgain.java
+++ src/T13PrefixAgain.java
@@ -3,9 +3,13 @@ public class T13PrefixAgain {
         String  str = "abXYabc";
         String prefix = "XY";
         int n = 2;
-
-
-
+         int count = 0;
+        for (int i = 0; i < str.length()-1;i++){
+            if (str.contains(prefix)){
+                count++;
+            }
+        }
+        System.out.println(count);
 
     }
 }
