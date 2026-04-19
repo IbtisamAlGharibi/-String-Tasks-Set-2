b6503f6be8e56e4d037699836066ccb99a489523
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 15:21:27 +0400

    if else statements are written inside the loop

--- src/T19StarOut.java
+++ src/T19StarOut.java
@@ -6,9 +6,26 @@ public class T19StarOut {
         String str = "ab*cd";
         String result = " ";
 
-
         for (int i = 0; i < str.length() - 1; i++) {
 
+            if (str.charAt(i) == '*') {
+                continue;
+            }
 
+            if (i > 0 && str.charAt(i - 1) == '*') {
+                continue;
+            }
+
+            if (i < str.length() - 1 && str.charAt(i + 1) == '*') {
+                continue;
+            }
+
+            result += str.charAt(i);
         }
-    }}
+
+        System.out.println(result);
+    }
+
+
+    }
+
