c2841c5def2558e4fe9a6f082f00035d75c66eaf
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 11:10:30 +0400

    str string is declared and for loop is created

---
+++ src/T7bPattern.java
@@ -0,0 +1,12 @@
+public class T7bPattern {
+
+    public static void main(String[] args){
+
+        String str = "abcbob";
+
+        for (int i =0; i<str.length()-1 ; i++){
+
+
+        }
+    }
+}
