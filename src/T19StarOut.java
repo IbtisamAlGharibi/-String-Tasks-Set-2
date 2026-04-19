b9aa5417c6d7e511b1ed3331208464480099f1d8
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 15:01:08 +0400

    string str and int star are declared

---
+++ src/T19StarOut.java
@@ -0,0 +1,10 @@
+import java.security.PublicKey;
+
+public class T19StarOut {
+    public static void main(String[] args){
+
+        String str = "ab*cd";
+        int star = str.indexOf("*");
+
+    }
+}
