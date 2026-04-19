ae8f2908d62e137a93a45f536edfdf78ec41035c
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 10:24:01 +0400

    the counter is declared and if statement is written inside for loop

--- src/T2HiCounter.java
+++ src/T2HiCounter.java
@@ -2,9 +2,11 @@ public class T2HiCounter {
     public static void main(String[] args){
         String str= "Hihihi";
 
+        int count = 0;
         for (int i =0; i<str.length();i++){
-
-
+            if (str.charAt(i)=='h' && str.charAt(i+10)=='i'){
+                count = + 1;
+            }
         }
 
     }
