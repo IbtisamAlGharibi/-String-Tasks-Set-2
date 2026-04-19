8f24abfa53c4b094bd560eabd65e77bacc0bd1ed
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 11:22:41 +0400

    2 strings are  declared and for loop is created

---
+++ src/T9StringMix.java
@@ -0,0 +1,14 @@
+public class T9StringMix {
+    public static void main(String[] args){
+        String str = "abc";
+        String str2 = "xyz";
+
+        for (int i =0; i<str.length()-1 ; i++){
+
+            }
+
+
+
+
+        }
+}
