fe13913e3d4bf72339eda0224d2f821adfee6e7d
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 14:27:17 +0400

    if statement is written to check if the before substring is equals to after substring

--- src/T16StarChar.java
+++ src/T16StarChar.java
@@ -1,11 +1,15 @@
 public class T16StarChar {
     public static void main(String[] args){
-        String str = "xy*yzz";
+        String str = "xy*xyyy";
         int first = str.indexOf("*");
 
         String before = str.substring(0,first);
         String after = str.substring(first+1 , str.length()-1);
 
-
+        if (before.equals(after)){
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
     }
 }
