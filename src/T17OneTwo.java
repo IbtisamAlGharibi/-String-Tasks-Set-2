73c0405df2cc7a4d3bc60df6fb4c40e834acd100
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 14:44:03 +0400

    the result is printed on the screen

--- src/T17OneTwo.java
+++ src/T17OneTwo.java
@@ -2,7 +2,9 @@ public class T17OneTwo {
     public static void main(String[] args){
         String str = "abc";
         char first = str.charAt(0);
+        char last = str.charAt(str.length()-1);
 
-
+        String result = last +" "+ str.charAt(1)+" " + first;
+        System.out.println(result);
     }
 }
