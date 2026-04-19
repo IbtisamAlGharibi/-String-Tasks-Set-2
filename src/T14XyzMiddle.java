01846faa9899416c4b093315305169e0b2ab4127
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 12:16:02 +0400

    Task14 xyz Middle

--- src/T14XyzMiddle.java
+++ src/T14XyzMiddle.java
@@ -2,16 +2,22 @@ public class T14XyzMiddle {
     public static void main(String[] args){
         String str = "AAxyzBB";
         String prefix = "xyz";
-      for (int i =0 ; i<str.length()-1;i++){
-            if (str.substring(i,i+3) == prefix){
 
-                int left= i;
-                int reight = str.length() -(i+3);
+        boolean found = false;
+        for (int i = 0; i <= str.length() - 3; i++){
+            if (str.substring(i, i + 3).equals(prefix)){
 
+                int left = i;
+                int right = str.length() - (i + 3);
 
+                if (Math.abs(left - right) <= 1){
+                    found = true;
+                    break;
+                }
             }
+        }
 
-      }
+        System.out.println(found);
 
     }
 }
