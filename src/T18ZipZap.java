9872f3a52c521128e6b4d007c357951c94063ee3
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 14:50:57 +0400

    str string are declared and empty result string is declare

---
+++ src/T18ZipZap.java
@@ -0,0 +1,12 @@
+public class T18ZipZap {
+    public static void main(String[] args){
+        String str = "zipXzap";
+        String result = "";
+
+        for (int i =0; i<str.length()-1;i++){
+
+
+        }
+
+    }
+}
