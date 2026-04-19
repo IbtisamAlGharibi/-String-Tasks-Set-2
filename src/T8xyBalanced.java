94d674c67a17522b368cd5f49015e90977fe89fd
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 11:18:22 +0400

    if statment is written to check the pattern

--- src/T8xyBalanced.java
+++ src/T8xyBalanced.java
@@ -3,7 +3,14 @@ public class T8xyBalanced {
         String str = "aaxbby";
 
         for (int i =0; i<str.length()-1 ; i++){
+            if (str.charAt(i)== 'x' && str.charAt(i+1)== 'y' ||
+                    str.charAt(i)== 'x' && str.charAt(i+2)== 'y' ||
+                    str.charAt(i)== 'x' && str.charAt(i+3)== 'y'){
 
+                System.out.println("True");
+            }else {
+                System.out.println("False");
+            }
 
         }
     }
