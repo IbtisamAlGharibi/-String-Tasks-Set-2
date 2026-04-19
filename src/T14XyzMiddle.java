8381a097780aa0c764f54a8c7e51414f00b771bc
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 12:11:16 +0400

    if statment is written to check if the prefix in the middle and left and rigth int declared

--- src/T14XyzMiddle.java
+++ src/T14XyzMiddle.java
@@ -3,7 +3,13 @@ public class T14XyzMiddle {
         String str = "AAxyzBB";
         String prefix = "xyz";
       for (int i =0 ; i<str.length()-1;i++){
+            if (str.substring(i,i+3) == prefix){
 
+                int left= i;
+                int reight = str.length() -(i+3);
+
+
+            }
 
       }
 
