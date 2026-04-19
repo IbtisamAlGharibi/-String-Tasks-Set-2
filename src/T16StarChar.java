a3cc7c39e35305a36f8fabaff36269c97db15859
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 14:24:53 +0400

    before and after strings are declared

--- src/T16StarChar.java
+++ src/T16StarChar.java
@@ -3,6 +3,9 @@ public class T16StarChar {
         String str = "xy*yzz";
         int first = str.indexOf("*");
 
+        String before = str.substring(0,first);
+        String after = str.substring(first+1 , str.length()-1);
+
 
     }
 }
