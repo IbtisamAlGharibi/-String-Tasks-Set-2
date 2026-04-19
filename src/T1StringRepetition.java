7bb02e9656b3680c654c2760ffc64158297fc0e6
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 10:13:33 +0400

    str string is declared and for loop is created

---
+++ src/T1StringRepetition.java
@@ -0,0 +1,13 @@
+public class T1StringRepetition {
+    public static void main(String[] args){
+
+        String str ="Ibtisam";
+
+        for (int i = 0; i < str.length(); i++){
+
+
+        }
+
+
+    }
+}
