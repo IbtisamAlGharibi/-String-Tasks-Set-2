c7b707e7806ec57cc8a14c3b50e01fc9059e5439
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 10:49:17 +0400

    if statement to count the number of code appearence

--- src/T4CodeCounter.java
+++ src/T4CodeCounter.java
@@ -5,8 +5,9 @@ public class T4CodeCounter {
         int count=0;
 
         for (int i =0 ; i < str.length()-1; i++){
-
-
+            if (str.charAt(i) == 'x'){
+                count++;
+            }
         }
 
 
