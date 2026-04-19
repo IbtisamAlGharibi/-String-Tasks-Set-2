0ba6f5b552f79e27237265de5144af2c47e2d064
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 12:07:52 +0400

    str and prefix strings are declared and for loop is created

---
+++ src/T14XyzMiddle.java
@@ -0,0 +1,11 @@
+public class T14XyzMiddle {
+    public static void main(String[] args){
+        String str = "AAxyzBB";
+        String prefix = "xyz";
+      for (int i =0 ; i<str.length()-1;i++){
+
+
+      }
+
+    }
+}
