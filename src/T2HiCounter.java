78c67a778dfd895be24609b018c437b3fc397247
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 10:29:24 +0400

    the counter is peinted on the screen

--- src/T2HiCounter.java
+++ src/T2HiCounter.java
@@ -1,13 +1,14 @@
 public class T2HiCounter {
-    public static void main(String[] args){
+   public static void main(String[] args){
+
         String str= "Hihihi";
 
         int count = 0;
-        for (int i =0; i<str.length();i++){
-            if (str.charAt(i)=='h' && str.charAt(i+10)=='i'){
+        for (int i =0; i < str.length() -1; i++){
+            if (str.charAt(i)=='h' && str.charAt(i+1)=='i'){
                 count = + 1;
             }
         }
-
+            System.out.println(count);
     }
 }
