9877ffaf620d966c2c388912ceb62ad1ef99f5a7
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 11:05:10 +0400

    if statement is written inside the loop to check the pattern

--- src/T6XYZCode.java
+++ src/T6XYZCode.java
@@ -3,7 +3,12 @@ public class T6XYZCode {
         String str = "abcxyz";
 
         for (int i =0; i < str.length()-1 ; i++){
-
+        if (str.charAt(i) =='.' && str.charAt(i+1) == 'x' && str.charAt(i+2) == 'y'
+                && str.charAt(i+3) == 'z'){
+            System.out.println("False");
+        }else {
+            System.out.println("True");
+        }
 
         }
 
