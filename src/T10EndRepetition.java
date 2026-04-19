af6ed426ff77b5310c1d4730a0c1df7e8faeab97
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 11:35:11 +0400

    substring is created for the string and printed n times

--- src/T10EndRepetition.java
+++ src/T10EndRepetition.java
@@ -3,6 +3,11 @@ public class T10EndRepetition {
         String str = "Hello";
         int n = 3;
 
+        String end = str.substring(str.length()-3, str.length());
+
+        for (int i =0; i<n ; i++){
+            System.out.println(end);
+        }
 
     }
 }
