f7893f9d34836aab9ac223d574a09ed28778dfe4
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 10:38:28 +0400

    2 if statements is written inside the loop

--- src/T3CatDogCounter.java
+++ src/T3CatDogCounter.java
@@ -6,6 +6,13 @@ public class T3CatDogCounter {
 
         for (int i=0; i < str.length()-1; i++){
 
+            if (str.charAt(i) == 'c' && str.charAt(i + 1) == 'a' && str.charAt(i + 2) == 't') {
+                catCount++;
+            }
+            if (str.charAt(i) == 'd' && str.charAt(i + 1) == 'o' && str.charAt(i + 2) == 'g'){
+                dogCount++;
+            }
+
 
         }
 
