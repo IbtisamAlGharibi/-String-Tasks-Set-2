07f5c1e04afda1a52a74b61d5f901949ea30d05d
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 15:04:08 +0400

    for loop is created

--- src/T19StarOut.java
+++ src/T19StarOut.java
@@ -1,10 +1,13 @@
 import java.security.PublicKey;
 
 public class T19StarOut {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         String str = "ab*cd";
         int star = str.indexOf("*");
 
+        for (int i =0; i<str.length()-1;i++){
+
+        }
     }
 }
