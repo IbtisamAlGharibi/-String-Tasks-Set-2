a45634cc643dbe6ac51d127fb1b59f41b0b61958
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-19 11:12:17 +0400

    if statment is written to check the pattern

--- src/T7bPattern.java
+++ src/T7bPattern.java
@@ -5,8 +5,11 @@ public class T7bPattern {
         String str = "abcbob";
 
         for (int i =0; i<str.length()-1 ; i++){
-
-
+            if (str.charAt(i)=='b' && str.charAt(i+2)=='b'){
+                System.out.println("True");
+            }else {
+                System.out.println("False");
+            }
         }
     }
 }
